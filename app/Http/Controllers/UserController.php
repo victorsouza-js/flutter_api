d73854479c1d78a42550cf316f9c4dd679307fdf
victorsouza-git <victorsouzaaa5611@gmail.com>
2025-05-26 23:53:11 -0300

    add

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->only([
+            'email',
+            'password'
+        ]);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Email ou senha inválido'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'token' => $token
+        ], Response::HTTP_OK);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Logout realizado com sucesso'
+        ], Response::HTTP_OK);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+
+            $token = $user->createToken($request->email)->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'token' => $token
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,15 @@
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::apiResource('/produto',ProdutoController::class);
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+
+
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[UserController::class,'logout']);
+    Route::apiResource('/produto',ProdutoController::class);
+});
