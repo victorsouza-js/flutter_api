0cf2345391ed758a6cce10293f3bd64776706cf3
victorsouza-git <victorsouzaaa5611@gmail.com>
2025-05-28 10:41:45 -0300

    add

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::post('/register',[UserController::class,'register']);
-Route::post('/login',[UserController::class,'login']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::apiResource('/produto', ProdutoController::class);
 
 
 
 
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout',[UserController::class,'logout']);
-    Route::apiResource('/produto',ProdutoController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
 });
